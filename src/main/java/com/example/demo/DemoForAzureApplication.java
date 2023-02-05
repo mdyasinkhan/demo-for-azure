eea93939f3b155b025ea1487b23cbb48be04ba6b
mdyasinkhan <65243270+mdyasinkhan@users.noreply.github.com>
2023-02-05 14:04:06 +0000

    first commit

---
+++ src/main/java/com/example/demo/DemoForAzureApplication.java
@@ -0,0 +1,22 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DemoForAzureApplication {
+	
+	@GetMapping("/result")
+	public String getResult() {
+		
+		return "This is just demo";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoForAzureApplication.class, args);
+	}
+
+}
